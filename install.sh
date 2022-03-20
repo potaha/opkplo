68045783b3a72631e5caf3a90c06dd006e3a02a0
potaha <84473573+potaha@users.noreply.github.com>
2022-03-20 14:22:26 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ dsrt=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
 if [ $dsrt = "Debian" ] ; then
 sudo apt-get -y install task-gnome-desktop
 elif [ $dsrt = "Ubuntu" ] ; then
-sudo apt install xfce4
+sudo apt -y install xfce4
 fi
 
     printf "\n${slm}-----> Kurulum Bitti....${normal}\n"
