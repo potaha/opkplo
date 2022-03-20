efa2e5ee5082fb246f9a352ebc7c918f75c2a2f9
potaha <84473573+potaha@users.noreply.github.com>
2022-03-20 13:59:54 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+    slm=$(echo "\033[31;3m")
+    normal=$(echo "\033[m")
+    menu=$(echo "\033[36m") #Blue
+    number=$(echo "\033[33m") #yellow
+    bgred=$(echo "\033[39;3m")
+    fgred=$(echo "\033[32;3m")
+    kbred=$(echo "\033[33;3m")
+    printf "\n${kbred}-----> Kurulum ilerliyor${normal}\n"
+#------------------------------------------------
+# Debian 10 Uzak Masaüstü Kurulumu
+#------------------------------------------------
+
+sudo apt update
+
+sudo apt -y install xrdp
+
+sudo adduser xrdp ssl-cert
+
+sudo ufw allow 3389
+
+sudo systemctl restart xrdp
+
+sudo apt-get -y install task-gnome-desktop
+
+    printf "\n${slm}-----> Kurulum Bitti....${normal}\n"
