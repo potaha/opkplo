bd411cc6327ca0683bc93891a17a4c033608acc6
potaha <84473573+potaha@users.noreply.github.com>
2022-03-20 15:33:01 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,17 +25,17 @@ sudo systemctl restart xrdp
 dsrt=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2 -d " ")
 system3=$(hostnamectl | egrep "Operating System" | cut -f2 -d":" | cut -f2-4 -d " ")
 if [ $system3 = "Debian GNU/Linux 9" ] ; then
-sudo apt install xfce4
+sudo apt -y install xfce4
 echo xfce4-session >~/.xsession 
-sudo apt-get install firefox 
+sudo apt-get -y install firefox 
 sudo service xrdp restart
 else
 if [ $dsrt = "Debian" ] ; then
 sudo apt-get -y install task-gnome-desktop
 elif [ $dsrt = "Ubuntu" ] ; then
-sudo apt install xfce4
+sudo apt -y install xfce4
 echo xfce4-session >~/.xsession 
-sudo apt-get install firefox 
+sudo apt-get -y install firefox 
 sudo service xrdp restart
 fi
 fi
